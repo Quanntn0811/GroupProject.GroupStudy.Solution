61c4c0276936924a9fe8b2173e39888d9755546f
datbh1420 <buihuydat207@gmail.com>
2023-06-27 15:35:21 +0700

    Add frmSlotManagement

---
+++ GroupStudyWinApp/frmGroupUser.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GroupStudyWinApp
+{
+    partial class frmGroupUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbfrmUserInGroup = new Label();
+            dataGridView1 = new DataGridView();
+            btnDelete = new Button();
+            btnClose = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // lbfrmUserInGroup
+            // 
+            lbfrmUserInGroup.AutoSize = true;
+            lbfrmUserInGroup.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            lbfrmUserInGroup.Location = new Point(297, 9);
+            lbfrmUserInGroup.Name = "lbfrmUserInGroup";
+            lbfrmUserInGroup.Size = new Size(194, 37);
+            lbfrmUserInGroup.TabIndex = 1;
+            lbfrmUserInGroup.Text = "User In Group";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 81);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(479, 325);
+            dataGridView1.TabIndex = 2;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(591, 221);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 3;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            btnClose.Location = new Point(591, 278);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(94, 29);
+            btnClose.TabIndex = 4;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = true;
+            // 
+            // frmGroupUser
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnClose);
+            Controls.Add(btnDelete);
+            Controls.Add(dataGridView1);
+            Controls.Add(lbfrmUserInGroup);
+            Name = "frmGroupUser";
+            Text = "frmGroupUser";
+            Load += frmGroupUser_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbfrmUserInGroup;
+        private DataGridView dataGridView1;
+        private Button btnDelete;
+        private Button btnClose;
+    }
+}
---
+++ GroupStudyWinApp/frmGroupUser.cs
@@ -0,0 +1,28 @@
+﻿using Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GroupStudyWinApp
+{
+    public partial class frmGroupUser : Form
+    {
+        IUserRepository userRepository = new UserRepository();
+        public int GroupId { get; set; }
+        public frmGroupUser()
+        {
+            InitializeComponent();
+        }
+
+        private void frmGroupUser_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
